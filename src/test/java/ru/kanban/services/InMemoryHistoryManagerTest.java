1abe695884099dbe624f271508cc22f523ec0f27
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-03-26 17:46:40 +0300

    24.4 Kanban part 4

---
+++ src/test/java/ru/kanban/services/InMemoryHistoryManagerTest.java
@@ -0,0 +1,50 @@
+package ru.kanban.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.kanban.models.Status;
+import ru.kanban.models.Task;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InMemoryHistoryManagerTest {
+
+    private InMemoryHistoryManager historyManager;
+
+    @BeforeEach
+    void setUp() {
+        historyManager = new InMemoryHistoryManager();
+    }
+
+    @Test
+    void addToHistory() {
+        Task task1 = new Task("title1", "description1", Status.NEW);
+        Task task2 = new Task("title2", "description2", Status.IN_PROGRESS);
+        historyManager.addToHistory(task1);
+        historyManager.addToHistory(task2);
+
+        List<Task> history = historyManager.getHistory();
+        assertThat(2).isEqualTo(history.size());
+        assertThat(task2).isEqualTo(history.get(0));
+        assertThat(task1).isEqualTo(history.get(1));
+    }
+
+    @Test
+    void addToHistorySizeLimit() {
+        for (int i = 0; i < 15; i++) {
+            Task task = new Task("title" + i, "description" + i, Status.IN_PROGRESS);
+            historyManager.addToHistory(task);
+        }
+
+        List<Task> history = historyManager.getHistory();
+        assertThat(10).isEqualTo(history.size());
+    }
+
+    @Test
+    public void getHistoryEmpty() {
+        List<Task> history = historyManager.getHistory();
+        assertThat(0).isEqualTo(history.size());
+    }
+}
