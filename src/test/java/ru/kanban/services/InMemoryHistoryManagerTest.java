147894ec6bf70fe624a9113fbb7fe8f1386c94ea
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-08-12 10:37:58 +0300

    31.12 Kanban part 5

--- src/test/java/ru/kanban/services/InMemoryHistoryManagerTest.java
+++ src/test/java/ru/kanban/services/InMemoryHistoryManagerTest.java
@@ -31,13 +31,36 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void addToHistorySizeLimit() {
-        for (int i = 0; i < 15; i++) {
-            Task task = new Task("title" + i, "description" + i, i, Status.IN_PROGRESS);
-            historyManager.addToHistory(task);
-        }
+    void removeTask() {
+        Task task1 = new Task("title1", "description1", 1, Status.NEW);
+        Task task2 = new Task("title2", "description2", 2, Status.IN_PROGRESS);
+        historyManager.addToHistory(task1);
+        historyManager.addToHistory(task2);
+        historyManager.remove(1);
         List<Task> history = historyManager.getHistory();
-        assertThat(15).isEqualTo(history.size());
+        assertThat(1).isEqualTo(history.size());
+        assertThat(task2).isEqualTo(history.get(0));
+    }
+
+    @Test
+    void addCopyTask() {
+        Task task = new Task("title1", "description1", 1, Status.NEW);
+        historyManager.addToHistory(task);
+        historyManager.addToHistory(task);
+        List<Task> history = historyManager.getHistory();
+        assertThat(1).isEqualTo(history.size());
+        assertThat(task).isEqualTo(history.get(0));
+    }
+
+    @Test
+    void addUpdateTask() {
+        Task task = new Task("title", "description", 1, Status.NEW);
+        Task taskUpdate = new Task("titleUpdate", "descriptionUpdate", 1, Status.IN_PROGRESS);
+        historyManager.addToHistory(task);
+        historyManager.addToHistory(taskUpdate);
+        List<Task> history = historyManager.getHistory();
+        assertThat(1).isEqualTo(history.size());
+        assertThat(taskUpdate).isEqualTo(history.get(0));
     }
 
     @Test
